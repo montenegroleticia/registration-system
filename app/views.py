13e96b9c2583637ad56ba6c5aea10fedabce6e1a
montenegroleticia <montenegro.letiicia@gmail.com>
2023-09-25 16:23:51 -0300

    User register

--- app/views.py
+++ app/views.py
@@ -1,2 +1,20 @@
 from django.shortcuts import render
+from .models import User
 
+
+def home(request):
+    return render(request, 'user/home.html')
+
+
+def users(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+
+        if name and age:
+            new_user = User(name=name, age=age)
+            new_user.save()
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'user/users.html', context)
--- registrationsystem/urls.py
+++ registrationsystem/urls.py
@@ -4,4 +4,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('users/', views.users, name='users'),
 ]
